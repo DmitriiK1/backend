debc15c676c0c162bf409c5cd8773df32d8c0f35
DmitriiK1 <dimonovec@mail.ru>
2020-06-22 12:45:46 +0300

    Исправил замечания

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
-Sprint 12 # backend
+# Sprint 12 # backend
 
 Выполнил настройку backend согласно заданию.
 
-ссылка на репозиторий
-https://github.com/DmitriiK1/backend
+[ссылка на репозиторий](https://github.com/DmitriiK1/backend)
 
 v0.0.1 version
--- routes/users.js
+++ routes/users.js
@@ -6,12 +6,7 @@ router.get('/', (req, res) => {
 });
 router.get('/:id', (req, res) => {
   const { id } = req.params;
-  const user = users.find((item) => {
-    if (item._id === id) {
-      return true;
-    }
-    return false;
-  });
+  const user = users.find((item) => item._id === id);
   if (!user) {
     res.status(404).json({ message: 'Нет пользователя с таким id' });
     return;
